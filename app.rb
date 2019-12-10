35f90b297783384805faf26d84e156db275a5e2e
Christopher Dent <christopherdent01@gmail.com>
2019-12-10 01:02:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,14 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-configure do
-  enable :sessions
-  set :session_secret, "magic"
-end
+  configure do
+    enable :sessions
+    set :session_secret, "magic"  
+  end
 
 
-get "/" do
+  get "/" do
   
-erb :index
+    erb :index
 
-end
+  end
+
+end 
